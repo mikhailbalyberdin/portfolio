04d64cab47b0623356d0e0726b7df1f42c4c16e9
Misha <mikhailbalyberdin@gmail.com>
2024-11-09 07:14:51 -0500

    feat: add event listener for header

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -5,10 +5,16 @@ const openClass = 'open';
 const eventHandler = (e) => {
     const isBurgerButton = e.target.closest('#button-burger');
     const mobileMenuElement = headerElement.querySelector('#mobile-menu');
+    const isLink = e.target.closest('[data-anchor-link]');
 
     if (isBurgerButton) {
         mobileMenuElement.classList.toggle(openClass);
     } 
+
+    if (isLink) {
+        console.log("Hello");
+        mobileMenuElement.classList.remove(openClass);
+    }
 }
 
 headerElement.addEventListener('click', eventHandler);
